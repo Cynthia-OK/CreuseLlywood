8b1aa4bf395be53e56efa906337409b3e4aaaa39
Marie2201 <gueye.etou@gmail.com>
2024-11-21 17:19:16 +0100

    m

--- test.py
+++ test.py
@@ -2,4 +2,6 @@ import pandas as pd
 
 tmdb_full = pd.read_csv("C:\Users\LOTUS\OneDrive\Documents\Formation_data_analyst\Cours\projets\projet2\Projet2\Data\TMDB\tmdb_full.csv")
 
-test
+test 
+
+print('hello')
