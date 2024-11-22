d6ab2be23a80f0e07fa95c188e92fa54a4c09854
Filiz-CENGIZ <filizcengiznarin@gmail.com>
2024-11-22 09:37:46 +0100

    coucou les filles

--- test.py
+++ test.py
@@ -2,4 +2,4 @@ import pandas as pd
 
 tmdb_full = pd.read_csv("C:\Users\LOTUS\OneDrive\Documents\Formation_data_analyst\Cours\projets\projet2\Projet2\Data\TMDB\tmdb_full.csv")
 
-test
+je suis fatiguees
