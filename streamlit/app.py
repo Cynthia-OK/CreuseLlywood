de14132e0b8c99d3d45f8fa1346097ef3344f9ed
Cynthia-OK <cynthia.hocquet@gmail.com>
2025-01-03 22:53:41 +0100

    changement accepté dans app

--- streamlit/app.py
+++ streamlit/app.py
@@ -6,34 +6,7 @@ st.set_page_config(
     page_icon="https://img.freepik.com/vecteurs-libre/concept-cinema_1284-12713.jpg", 
     layout="wide", 
     initial_sidebar_state="expanded"
-<<<<<<< HEAD
 )
-=======
-) 
-
-
-pages = {
-    "Accueil" : [
-        st.Page("./pages/accueil.py", title="Accueil", icon="🏠"),
-        ],
-    "Statistiques": [
-        st.Page("./pages/statistiques.py", title="Page de statistiques", icon="📊"),
-    ],
-    "Recommandations": [
-        st.Page("./pages/recommandation.py", title="Recherche de films similaires par titre", icon="🎞️"),
-        st.Page("./pages/recherche_directeur.py", title="Recherche de films par directeur", icon="📽️"),
-        st.Page("./pages/recherche_acteur.py", title="Recherche de films par acteur", icon="🎭"),
-        st.Page("./pages/recherche_genre.py", title="Recherche de films par genre", icon="💥"),
-        st.Page("./pages/recherche_annee.py", title="Recherche de films par année", icon="📅"),
-        
-    ],
-}
-
-pg = st.navigation(pages)
-pg.run()
-
-st.sidebar.image("http://blog.ac-versailles.fr/cineblog/public/cinema.jpg")
->>>>>>> parent of 11ee527 (changement lien image sidebar)
 
 # CSS personnalisé
 sidebar_css = """
@@ -75,7 +48,6 @@ sidebar_css = """
 """
 
 # Injecter le CSS dans l'application Streamlit
-<<<<<<< HEAD
 st.sidebar.markdown(sidebar_css, unsafe_allow_html=True)
 
 # Ajouter un titre ou un texte au-dessus du menu via HTML
@@ -109,6 +81,3 @@ pages = {
 
 pg = st.navigation(pages)
 pg.run()
-=======
-st.markdown(sidebar_css, unsafe_allow_html=True)
->>>>>>> parent of 11ee527 (changement lien image sidebar)
